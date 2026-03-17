7ab46467caab6344f9008150cf14b3fd87319e62
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-17 19:48:21 -0300

    Adiciona chamada da LoginScreen na MainActivity como tela inicial do app

--- app/src/main/java/com/example/navigationbetweenscreens_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/navigationbetweenscreens_kotlin/MainActivity.kt
@@ -7,10 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigationbetweenscreens_kotlin.screens.LoginScreen
 import com.example.navigationbetweenscreens_kotlin.ui.theme.NavigationBetweenScreensKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavigationBetweenScreensKotlinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
