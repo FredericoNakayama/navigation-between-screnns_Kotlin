1088ecac8b6cbedc8e1c7213042c16bc7b7baae6
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-24 21:34:11 -0300

    Passagem de parâmetros obrigatórios na tela de Perl+c r iação checkpoint1.d

---
+++ app/src/main/java/com/github/FredericoNakayama/navigationbetweenscreens_kotlin/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.github.FredericoNakayama.navigationbetweenscreens_kotlin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.github.FredericoNakayama.navigationbetweenscreens_kotlin.screens.LoginScreen
+import com.github.FredericoNakayama.navigationbetweenscreens_kotlin.screens.MenuScreen
+import com.github.FredericoNakayama.navigationbetweenscreens_kotlin.screens.PedidosScreen
+import com.github.FredericoNakayama.navigationbetweenscreens_kotlin.screens.PerfilScreen
+import com.github.FredericoNakayama.navigationbetweenscreens_kotlin.ui.theme.NavigationBetweenScreensKotlinTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationBetweenScreensKotlinTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                    ) {
+                        composable(route = "login") {
+                            LoginScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "menu") {
+                            MenuScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "pedidos") {
+                            PedidosScreen(modifier = Modifier.padding(innerPadding), navController)
+                        }
+                        composable(route = "perfil/{nome}") {
+                            val nome: String? = it.arguments?.getString("nome", "Usuário Genérico")
+                            PerfilScreen(modifier = Modifier.padding(innerPadding), navController, nome!!)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
